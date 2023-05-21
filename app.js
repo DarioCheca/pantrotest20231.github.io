240c8ad7c44fa411e46d24a703b79633b2ffd94e
Dario Checa <31047282+DarioCheca@users.noreply.github.com>
2023-05-21 13:29:18 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  var descripcion = document.getElementById('descripcion').value;
+
+  var fotoInput = document.getElementById('foto');
+  var foto = fotoInput.files[0];
+
+  if (foto) {
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      var fotoDataUrl = event.target.result;
+      
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var latitud = position.coords.latitude;
+        var longitud = position.coords.longitude;
+
+        // Hacer algo con la descripción, la foto y las coordenadas geográficas
+        // por ejemplo, enviar los datos al servidor mediante una petición AJAX
+
+        // Reiniciar el formulario
+        document.getElementById('descripcion').value = '';
+        fotoInput.value = '';
+      }, function(error) {
+        console.log('Error al obtener la ubicación:', error);
+      });
+    };
+    reader.readAsDataURL(foto);
+  }
+});
+
